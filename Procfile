5a0bbd7f820efde72c1691cfdd661766ca332ac5
Asanali <asanali1@mail.ru>
2018-07-30 14:35:16 +0600

    init_2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn simpleform.wsgi --log-file -
--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+django_heroku.settings(locals())
 
 BOOTSTRAP3 = {
     'include_jquery': True,
