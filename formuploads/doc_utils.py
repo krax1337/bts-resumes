822b568385eadf405ca1950db70dca4e48a1b9ec
Asanali <asanali1@mail.ru>
2018-08-01 02:00:42 +0600

    format_3

--- formuploads/doc_utils.py
+++ formuploads/doc_utils.py
@@ -2,12 +2,14 @@ from subprocess import Popen, PIPE
 import os
 import shutil
 
+
 def doc_to_text(file_path):
     file_path = file_path[:-4]
     coding = "-mcp1251"
-    cmd = 'antiword' + " " +coding + " " ' "./' + file_path + ".doc"+ '"' + " " + ">" + " ./upload/" + "new.txt"
+    cmd = 'antiword' + " " + coding + " " ' "./' + file_path + \
+        ".doc" + '"' + " " + ">" + " ./upload/" + "new.txt"
     os.system(str(cmd))
-    f = open('./upload/new.txt','r')
+    f = open('./upload/new.txt', 'r')
     result = []
     for line in f:
         if line != '\n':
