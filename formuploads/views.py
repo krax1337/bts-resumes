ca309dc1fff732fb578b6fc9de25459d1d57b03d
Amina <amina.shamil555@gmail.com>
2018-08-02 18:23:48 +0600

    all jobs

--- formuploads/views.py
+++ formuploads/views.py
@@ -1,7 +1,7 @@
 import json
 import os
 import shutil
-import urllib
+import urllib.request
 from xml.etree import ElementTree as ET
 
 from dicttoxml import dicttoxml
@@ -12,7 +12,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .vacants import get_vacants
 
 cv_summary = {}
-
+all_jobs=dict()
 
 def home(request):
     return render(request, 'formuploads/index.html', {'what': 'Upload your CV'})
@@ -94,3 +94,32 @@ def analyse_file(all_vacants_info, vacants_ids):
 
                 })
     return all_vacants_info
+
+def all_jobs():
+    all_jobs=[{}]
+    requestURL = "https://www.enbek.kz/ru/xml/jooble"
+    root = ET.parse(urllib.request.urlopen(requestURL)).getroot()
+    
+    for job in root.iter('job'):
+        
+        all_jobs.append({
+
+                        'job_name': str(job.find('name').text).replace(", ", "", 1),
+                        'job_region': str(job.find('region').text),
+                        "job_salary": str(job.find('salary').text),
+                        "job_description": str(job.find('description').text).replace("p&gt;", " ")
+                                    .replace("li", " ").replace("ul", " ")
+                                    .replace("/", " ").replace("&gt;", " ")
+                                    .replace("&lt;", " ").replace("ul&gt;", " ")
+                                    .replace("/li&gt;", " ").replace("li&gt;", " ")
+                                    .replace("-&amp;", " ").replace("nbsp;", " ")
+                                    .replace("&amp;", " ").replace("quot;", " ")
+                                    .replace("br", " ").replace("strong", " ")
+                                    .replace("strong", " ").replace("ol", " "),
+                        "job_email": str(job.find('email').text),
+                        "job_phone": str(job.find('phone').text),
+                        "job_link": str(job.find('link').text),
+
+                    })
+    return all_jobs   
+
