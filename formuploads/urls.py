8700c51b0d93f772b414a0510f7a2aca29bd3749
Asanali <asanali1@mail.ru>
2018-08-03 23:21:52 +0600

    ui edit_1

--- formuploads/urls.py
+++ formuploads/urls.py
@@ -8,9 +8,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('upload/', views.upload, name="upload"),
     path('search/', views.search_v, name="search"),
-    path('upload/test/', views.test, name="test"),
-   
     path('upload/show_json/', views.show_json, name="show_json"),
     path('upload/show_xml/', views.show_xml, name="show_xml"),
-    path('upload/rate/', views.rate, name = 'rate')
+    path('upload/rate/', views.rate, name = 'rate'),
+    path('upload/rate/test', views.test, name="test"),
 ]
