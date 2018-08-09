6d30718962c6eab47a3d3cc617116985e0044bd0
Asanali <asanali1@mail.ru>
2018-08-09 23:14:13 +0600

    some delete

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,10 +126,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
-
 django_heroku.settings(locals())
 
 # BOOTSTRAP3 = {
