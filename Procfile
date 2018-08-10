ab4b7c4e37036515f9d7d1281953485ea9721996
Asanali <asanali1@mail.ru>
2018-08-10 13:11:03 +0600

    fix limits

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn myproject.wsgi --log-file -
+web: gunicorn myproject.wsgi --limit-request-line 20000
--- static/js/generate_pdf.js
+++ static/js/generate_pdf.js
@@ -1,5 +1,10 @@
 var i_ed = 0;
 var i_ex = 0;
+var i_la = 0;
+
+function increment_lan() {
+    i_la += 1; /* function for automatic increament of feild's "Name" attribute*/
+}
 
 function increment_edu() {
     i_ed += 1; /* function for automatic increament of feild's "Name" attribute*/
@@ -79,6 +84,68 @@ function new_education() {
 
     document.getElementById("i_ed").value = i_ed;
     document.getElementById("educations").appendChild(r);
+}
+
+
+function new_experinece() {
+    var r = document.createElement('div');
+    exp += 1;
+    r.setAttribute("id", 'exp' + exp);
+
+    r.setAttribute("class", 'element_ed');
+    increment_exp();
+
+    var y = document.createElement("INPUT");
+    y.setAttribute("type", "text");
+    y.setAttribute("placeholder", "Название компании");
+    y.setAttribute("class", "form-control");
+    y.setAttribute("style", " text-align:center");
+    y.setAttribute("name", "experience_name" + i_ex);
+    y.setAttribute("id", i_ex);
+    y.required = true;
+
+
+    var y_2 = document.createElement("INPUT");
+    y_2.setAttribute("type", "text");
+    y_2.setAttribute("placeholder", "Должность");
+    y_2.setAttribute("class", "form-control");
+    y_2.setAttribute("style", " text-align:center");
+    y_2.setAttribute("name", "experience_designation" + i_ex);
+    y_2.setAttribute("id", i_ex);
+
+    var y_3 = document.createElement("INPUT");
+    y_3.setAttribute("type", "text");
+    y_3.setAttribute("placeholder", "Начало работы");
+    y_3.setAttribute("class", "form-control");
+    y_3.setAttribute("style", " text-align:center");
+    y_3.setAttribute("name", "experience_start_date" + i_ex);
+    y_3.setAttribute("id", i_ex);
+
+    var y_4 = document.createElement("INPUT");
+    y_4.setAttribute("type", "text");
+    y_4.setAttribute("placeholder", "Конец работы");
+    y_4.setAttribute("class", "form-control");
+    y_4.setAttribute("style", " text-align:center");
+    y_4.setAttribute("name", "experience_end_date" + i_ex);
+    y_4.setAttribute("id", i_ex);
+
+    var y_5 = document.createElement("TEXTAREA");
+    y_5.setAttribute("type", "text");
+    y_5.setAttribute("placeholder", "Описание");
+    y_5.setAttribute("class", "form-control");
+    y_5.setAttribute("style", " text-align:center");
+    y_5.setAttribute("name", "experience_description" + i_ex);
+    y_5.setAttribute("id", i_ex);
+
+
+    r.appendChild(y);
+    r.appendChild(y_2);
+    r.appendChild(y_3);
+    r.appendChild(y_4);
+    r.appendChild(y_5);
+
+    document.getElementById("i_ex").value = i_ex;
+    document.getElementById("experiences").appendChild(r);
 }
 
 
